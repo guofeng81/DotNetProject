1a571dbb97796f45019ea3c89feb135dd220804d
Feng Guo <fguo81@gmail.com>
2019-09-22 17:07:48 -0500

    Initial check-in of module NG_Core_Auth

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NG_Core_Auth.Data;
+using NG_Core_Auth.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/values
+        [HttpGet("[action]")]
+        [Authorize(Policy = "RequiredLoggedIn")]
+        public IActionResult GetProducts()
+        {
+            return Ok(_db.Products.ToList());
+        }
+
+
+        [HttpPost("[action]")]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> AddProduct([FromBody] ProductModel formdata)
+        {
+            var newproduct = new ProductModel
+            {
+                Name = formdata.Name,
+                Description = formdata.Description,
+                ImageUrl = formdata.ImageUrl,
+                OutOfStock = formdata.OutOfStock,
+                Price = formdata.Price
+
+            };
+
+            await _db.Products.AddAsync(newproduct);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Product was Added Successfully"));
+
+        }
+
+        [HttpPut("[action]/{id}")]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] int id, [FromBody] ProductModel formdata)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var findProduct = _db.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (findProduct == null)
+            {
+                return NotFound();
+            }
+
+            // if the product is found
+            findProduct.Name = formdata.Name;
+            findProduct.Description = formdata.Description;
+            findProduct.ImageUrl = formdata.ImageUrl;
+            findProduct.OutOfStock = formdata.OutOfStock;
+            findProduct.Price = formdata.Price;
+
+            _db.Entry(findProduct).State = EntityState.Modified;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Prodct with id " + id + " is updated"));
+        }
+
+
+        [HttpDelete("[action]/{id}")]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var findProduct = await _db.Products.FindAsync(id);
+
+            if (findProduct == null)
+            {
+                return NotFound();
+            }
+
+            // if the product is found
+            _db.Products.Remove(findProduct);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult("The Prodct with id " + id + " is Deleted"));
+        }
+
+    }
+}
